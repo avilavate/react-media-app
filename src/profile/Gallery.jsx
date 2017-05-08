abb5014d9bbad10f5282ac748786f482d5224794
avilavate <avilavate123@gmail.com>
2017-05-08 11:02:02 +0530

    Fixed loop in gallery

--- src/profile/Gallery.jsx
+++ src/profile/Gallery.jsx
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 import '../profile/Gallery.css';
 
+//ToDo: Remove Sample data once tested
+import artist from '../sample-data.js';
 
 import { Grid, Col, Row } from 'react-bootstrap';
 
 class Gallery extends Component {
     componentWillMount() {
         this.state = {
-            albums: undefined
+            albums: { items: [] }
         };
     }
 
-    updateAlbums() {
-        console.dir(this.props.artist);
-        if (!this.props.artist) return;
+    componentWillReceiveProps() {
+        if (!this.props.artist) {
+            this.setState({ albums: { items: [] } });
+        } else
+            this.state.albums.items.forEach(function (album) {
+                this.state.albums.push({
+                    artistImageUrl: album.images[0].url ? album.images[0].url : '',
+                    // followers: album.followers.total ? album.followers.total : 0,
+                    name: album.name ? album.name : '',
+                    genres: album.genres ? album.genres : []
+                })
+            }, this);
 
-        this.state.albums = this.props.artist.albums || [];
-        albums.items.forEach(function (album) {
-            this.state.albums.push({
-                artistImageUrl: album.images[0].url ? album.images[0].url : '',
-                // followers: album.followers.total ? album.followers.total : 0,
-                name: album.name ? album.name : '',
-                genres: album.genres ? album.genres : []
-            })
-        }, this);
-        
     }
 
     render() {
-        this.updateAlbums();
         let albumCols = [];
         console.log("Gallery");
-        console.dir(this.props);
-        this.state.albums.forEach((album, k) => {
+        console.dir(this.state.albums);
+        if (!this.state.albums && this.state.albums.items.length < 1) return;
+        this.state.albums.items.forEach((album, k) => {
             albumCols.push(<Row key={k} className="show-grid Profile-Info">
                 <Col sm={12} md={2}><img
                     className="Profile-Image"
