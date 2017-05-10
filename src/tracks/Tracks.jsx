baf19e0c2a877e2ad87fe74091fec182024278d1
avilavate <avilavate123@gmail.com>
2017-05-10 17:10:51 +0530

    Fixed play/pause toggling and icon css.

--- src/tracks/Tracks.jsx
+++ src/tracks/Tracks.jsx
@@ -12,15 +12,32 @@ class Tracks extends Component {
             play: 'play',
             pause: 'pause',
             glyphIcon: [],
-            hide: []
+            hide: [],
+            length: 0
         }
     }
     playPause(k) {
-        let newGlyphIcon = this.state.glyphIcon;
+        debugger;
+        let newGlyphIcon = this.state.glyphIcon.slice();
+        let newHide = [];
         newGlyphIcon[k] === this.state.play ?
-            newGlyphIcon[k] = this.state.pause :
-            newGlyphIcon[k] = this.state.play;
-        this.setState({ glyphIcon: newGlyphIcon });
+            newGlyphIcon[k] = this.state.pause : newGlyphIcon[k] = this.state.play;
+
+        if (newGlyphIcon[k] === this.state.pause) {
+            for (let i = 0; i < this.state.glyphIcon.length; i++) {
+                if (i !== k) {
+                    newGlyphIcon[i] = this.state.play;
+                    newHide[i] = true;
+                }
+                else {
+                    newHide[i] = false;
+                }
+            }
+        }
+        this.setState({
+            glyphIcon: newGlyphIcon,
+            hide: newHide
+        });
     }
 
     initializeHide(length) {
@@ -35,14 +52,14 @@ class Tracks extends Component {
         }
     }
     shouldHide(k) {
-        let newHide = this.state.hide;
+        let newHide = this.state.hide.slice();
         newHide[k] = true;
         if (this.state.glyphIcon[k] === this.state.pause) newHide[k] = false;
         this.setState({ hide: newHide });
-        console.dir("hide array", this.state.hide)
+        console.log("hide array", this.state.hide)
     }
     shouldShow(k, length) {
-        let newHide = this.state.hide;
+        let newHide = this.state.hide.slice();
         newHide[k] = false;
         this.setState({ hide: newHide });
         console.dir("hide array", this.state.hide)
@@ -50,20 +67,23 @@ class Tracks extends Component {
     getTrackUi(ts) {
         let trackRows = ts.map((t, k) => {
             return <div style={{ flex: 1 }} key={k} className="PlayPause">
-                <div style={{ 'backgroundImage': `url(${t.trackImageUrl})` }}
+                <div style={{
+                    backgroundImage: `url(${t.trackImageUrl})`,
+                    backgroundSize: '120px 120px'
+                }}
                     alt={k + "_Track"}
                     className="Track-Image"
                     onMouseEnter={this.shouldShow.bind(this, k, ts.length)}
                     onMouseLeave={this.shouldHide.bind(this, k, ts.length)}
                 >
                     <span className={this.state.hide[k] ? 'Hidden' : 'AudioIcon'}>
-                        <Glyphicon glyph={this.state.glyphIcon[k] || this.state.play} onClick={this.playPause.bind(this, k)} />
+                        <Glyphicon glyph={this.state.glyphIcon[k]} onClick={this.playPause.bind(this, k)} />
                     </span>
                 </div>
                 <p style={{ float: 'center' }}>
                     <span>{t.name}</span>
                 </p>
-            </div>
+            </div >
         });
 
         return trackRows;
@@ -75,12 +95,10 @@ class Tracks extends Component {
         }
     }
     render() {
-
-        console.log("render", this.props);
         if (!this.props.Tracks) return null
         let tracks = [];
 
-        this.props.Tracks.tracks.map(t => {
+        this.props.Tracks.tracks.forEach(t => {
             tracks.push({
                 trackImageUrl: t.album.images[0].url,
                 name: t.album.name,
